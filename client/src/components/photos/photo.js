0394bf8db4b9ba9d3061f3f7a7ce5bcafb5289d5
sephson <disutj@gmail.com>
2021-10-28 13:24:13 +0100

    adaptive photo layout

--- client/src/components/photos/photo.js
+++ client/src/components/photos/photo.js
@@ -1,5 +1,37 @@
 import React from "react";
+import "./photos.css";
+import one from "../../pictures/1.jpg";
+import two from "../../pictures/2.jpg";
+import three from "../../pictures/3.jpg";
+import four from "../../pictures/4.jpg";
+import five from "../../pictures/5.jpg";
+import six from "../../pictures/6.jpg";
+import seven from "../../pictures/7.jpg";
+import eight from "../../pictures/8.jpg";
 
 export const Photo = () => {
-  return <div>PHOTOS</div>;
+  const pics = [
+    { postedBy: "Disu", src: one },
+    { postedBy: "Toyin", src: two },
+    { postedBy: "Mic", src: three },
+    { postedBy: "Anu", src: four },
+    { postedBy: "Disu", src: five },
+    { postedBy: "Toyin", src: six },
+    { postedBy: "Mic", src: seven },
+    { postedBy: "Anu", src: eight },
+  ];
+  return (
+    <div className="photos">
+      <ul className="gallery-container">
+        {pics.map((pic) => {
+          return (
+            <li className="image-container">
+              <img className="gallery-image" src={pic.src} alt="gallery" />
+              {/* <p>{pic.postedBy}</p> */}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
